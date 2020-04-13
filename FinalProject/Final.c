916c4a89ed9c47cb797388cca1c882c0761dc9a0
Zanne Hanna <shanna@berklee.edu>
2020-04-13 12:38:40 -0400

    Final Update 2
    DFT updated

--- FinalProject/Final.c
+++ FinalProject/Final.c
@@ -8,20 +8,20 @@
 
 //How to compile: clang Final.c -o Final libattopng.c -lsndfile 
 
-void DFT (double *samples, int N, double *X);
-void DFT (double *samples, int N, double *X)
+void DFT (double *DFTIn, double *DFTOut, int N);
+void DFT (double *DFTIn, double *DFTOut, int N)
 {
-    for (int k = 0; k < N; k++)
+    for (int k = 0; k < N - 1; k++)
     {
-        for (int n = 0; n < N; n++)
+        for (int n = 0; n < N - 1; n++)
         {
-            X[k] = samples[n] * (cos ((2 * M_PI / N) * k * n) - sin ((2 * M_PI / N) * k * n));
+            DFTOut[k] = DFTIn[n] * (cos (((2 * M_PI) / N) * k * n) - sin (((2 * M_PI) / N) * k * n));
         }
     }
 }
 
-void PNG (float *X, int N);
-void PNG (float *X, int N)
+void PNG (double *X, int N);
+void PNG (double *X, int N)
 {
     libattopng_t *png = libattopng_new(N*2, N*2, PNG_GRAYSCALE);
     
@@ -35,4 +35,56 @@ void PNG (float *X, int N)
 
     libattopng_save (png, "test_DFT.png");
     libattopng_destroy (png);
+}
+
+int main() 
+{
+    SNDFILE *sndFile;
+	SF_INFO sfInfo;
+    double *buffer;
+    double *DFTOut;
+    
+    memset(&sfInfo, 0, sizeof(SF_INFO));
+
+    sndFile = sf_open("sine.wav", SFM_READ, &sfInfo);
+
+    if(!sndFile)
+    {
+        printf ("Error : could not open file.\n");
+		puts (sf_strerror (NULL));
+		return 1;
+    }
+
+    long long N = sfInfo.frames;
+
+    buffer = malloc (N * sizeof(double));
+
+    if (!buffer)
+    {
+        printf ("Error : Malloc failed.\n");
+		return 1;
+	}
+
+    DFTOut = malloc (N * sizeof(double));
+
+    if (!DFTOut)
+    {
+        printf ("Error : Malloc failed.\n");
+		return 1;
+	}
+
+    //DFT (buffer, DFTOut, N);
+
+    //PNG (DFTOut, N);
+
+    if(!sf_format_check(&sfInfo))
+    {	
+        sf_close(sndFile);
+		printf("Invalid encoding\n");
+		return 1;
+	}
+
+    free (buffer);
+    free (DFTOut);
+    return 0;
 }
