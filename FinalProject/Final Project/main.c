8379f19d00747ba215385d211a4cc248bee4c89e
Zanne Hanna <shanna@berklee.edu>
2020-05-02 17:23:48 -0400

    fft update
    fft update

--- FinalProject/Final Project/main.c
+++ FinalProject/Final Project/main.c
@@ -7,19 +7,84 @@
 
 #define BUFFERSIZE 256
 
-//How to compile: clang Final.c -o Final libattopng.c -lsndfile 
-//How to compile: clang Final.c -o Final -lsndfile
+typedef struct complex
+{
+    double real;
+    double imag;
+} complex;
+
+//How to compile: clang main.c -o main -lsndfile
+
+void partition (complex *inStart, complex *inEnd);
+void partition (complex *inStart, complex *inEnd)
+{
+    int N = inEnd-inStart;
+    complex *temp;
+    temp = malloc(N * sizeof(complex));
+
+    //Separating odd and even items into temp.
+    for (int i = 0; i < N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            temp[(int)floor(i/2)] = inStart[i];
+        }
+        else
+        {
+            temp[(N/2) + (int)floor(i/2)] = inStart[i];
+        }
+        
+    }
+    //Putting temp into inStart in the new order.
+    for (int i = 0; i < N; i++)
+    {
+        inStart[i] = temp[i];
+    }
+    free (temp);
+}
+
+void FFT (complex *inStart, complex *inEnd);
+void FFT (complex *inStart, complex *inEnd)
+{
+    int N = inEnd - inStart;
+
+    if (N < 2) return;
+
+    else 
+    {
+        partition (inStart, inEnd);
+    }
+
+    FFT (inStart, inStart + (N/2));
+    FFT (inStart + (N/2), inEnd);
+
+    for (int i = 0; i < (N/2); i++) 
+    {
+        complex even = *(inStart + i);
+        complex odd = *(inStart + (N/2) + i);
+        complex dft = {0, exp(-2.0 * M_PI * i / N)};
+        dft.real = (dft.real * odd.real) + (dft.imag *odd.imag);
+        dft.imag = (dft.real * odd.imag) + (dft.imag * odd.real); //mafs
+
+        *(inStart + i) = even;
+        (inStart + i)->real += dft.real;
+        (inStart + i)->imag += dft.imag;
+        *(inStart + (N/2) + i) = even;
+        (inStart + (N/2) + i)->real -= dft.real;
+        (inStart + (N/2) + i)->imag -= dft.imag;
+    }
+}
 
 int main() 
 {
     SNDFILE *sndFile;
 	SF_INFO sfInfo;
-    double *buffer;
-    double *DFTOut;
+    complex *buffer;
+    complex *FFTOut;
     
     memset(&sfInfo, 0, sizeof(SF_INFO));
 
-    sndFile = sf_open("00-calung1.wav", SFM_READ, &sfInfo);
+    sndFile = sf_open("sine.wav", SFM_READ, &sfInfo);
 
     if(!sndFile)
     {
@@ -30,7 +95,7 @@ int main()
 
     long long N = sfInfo.frames;
 
-    buffer = malloc (N * sizeof(double));
+    buffer = malloc (N * sizeof(complex));
 
     if (!buffer)
     {
@@ -38,23 +103,32 @@ int main()
 		return 1;
 	}
 
-    DFTOut = malloc (BUFFERSIZE * sizeof(double));
+    FFTOut = malloc (BUFFERSIZE * sizeof(complex));
 
-    if (!DFTOut)
+    if (!FFTOut)
     {
         printf ("Error : Malloc failed.\n");
 		return 1;
 	}
 
-    sf_read_double (sndFile, buffer, N);
-    
-    DFT (buffer, DFTOut, N);
+    buffer = malloc (sizeof(buffer));
 
-    printf ("DFT done.\n");
+
+    buffer->real = malloc (BUFFERSIZE * sizeof(buffer->real));
+
+    buffer->imag = malloc (BUFFERSIZE * sizeof(buffer->imag));
+ 
+
+    sf_read_double (sndFile, buffer->real, N);
+    sf_read_double (sndFile, buffer->imag, N);
+    
+    FFT (buffer, FFTOut);
+
+    printf ("FFT done.\n");
     
     for (int p = 0; p < BUFFERSIZE; p++)
     {
-        printf("DFT Value: %lf\n", DFTOut[p]);
+        printf("FFT Value: %lf, %lf\n", FFTOut[p].real, FFTOut[p].imag);
     }
 
     if(!sf_format_check(&sfInfo))
@@ -65,6 +139,8 @@ int main()
 	}
 
     free (buffer);
-    free (DFTOut);
+    free (FFTOut);
     return 0;
 }
+
+//store results of fft into csb file and graph it there
